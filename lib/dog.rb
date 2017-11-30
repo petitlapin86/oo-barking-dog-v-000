c80034a6a18210f15ae8a15684ce83d7fa906f9f
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-30 18:13:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,6 @@
 # Your code goes here!
 class Dog
-
+  def name=(dog_name) # this is an instance method that takes an argument
+     @name = dog_name # Im setting my instance to equal a Variable
+   end
 end 
