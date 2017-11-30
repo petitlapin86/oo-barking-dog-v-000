e5e872863284aa09bd2be158ef311a038f86a289
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-30 18:15:03 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Dog
+  def name=(dog_name) # this is an instance method that takes an argument
+     @name = dog_name # Im setting my instance to equal a Variable
+   end
+
+   def name #this method is reporting or reading the name AKA GETTER METHOD
+   @name #this is an instance variable
+ end
+
+ def bark
+   puts "woof!"
+ end
+
+end
